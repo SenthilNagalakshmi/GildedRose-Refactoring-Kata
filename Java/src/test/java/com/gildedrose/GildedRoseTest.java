905782f45efc23fe4c8ccbef0b00e68c74d867fe
SenthilNagalakshmi <63703212+SenthilNagalakshmi@users.noreply.github.com>
2023-09-11 19:04:30 +0530

    new feature addition,test coverage changes

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,30 @@
-package com.gildedrose;
+package com.glidedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.approvaltests.combinations.CombinationApprovals;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    void updateQuality() throws Exception{
+    	
+    	CombinationApprovals.verifyAllCombinations(this::doUpdateQuality, new String[] {"Aged Brie","Backstage passes to a TAFKAL80ETC concert",
+    			"Sulfuras, Hand of Ragnaros","Conjured"},
+    	         new Integer[] {-1,0,2,6,11},
+    	         new Integer[] {0,1,49,50}
+    	);
+       
+    }
+    
+    private  String doUpdateQuality(String name,int sellIn,int quality)
+    {
+    	 Item[] items = new Item[] { new Item(name, sellIn, quality) };
+         GildedRose app = new GildedRose(items);
+         app.updateQuality();
+		return app.items[0].toString();
+    	
     }
 
 }
